a7ba39a55c0fb5e055be4d746df380b810fcd349
Anton Smirnov <anton@adblockplus.org>
2018-05-04 23:45:15 +0500

    Issue #6583 - Make sure files have correct license headers
    Replaced with eyeo's header.

--- chrome/browser/android/adblock/adblock_bridge.h
+++ chrome/browser/android/adblock/adblock_bridge.h
@@ -1,6 +1,19 @@
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CHROME_BROWSER_ANDROID_ADBLOCK_BRIDGE_H_
 #define CHROME_BROWSER_ANDROID_ADBLOCK_BRIDGE_H_
